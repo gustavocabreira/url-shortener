7cb87c7a5e40517524e7d378340ebebe97d1dd22
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-06-05 01:33:03 -0300

    refactor: add form request

--- app/Http/Controllers/Api/CurrentSessionController.php
+++ app/Http/Controllers/Api/CurrentSessionController.php
@@ -5,21 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CurrentSession\LoginRequest;
 use App\Http\Resources\AccessTokenResource;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 final class CurrentSessionController extends Controller
 {
-    public function store(Request $request): JsonResponse
+    public function store(LoginRequest $request): JsonResponse
     {
-        $validated = $request->validate([
-            'email' => ['required', 'string'],
-            'password' => ['required', 'string'],
-        ]);
-
-        if (! auth()->attempt($validated)) {
+        if (! auth()->attempt($request->validated())) {
             return response()->json(['message' => 'The provided credentials are incorrect.'], Response::HTTP_UNAUTHORIZED);
         }
 
