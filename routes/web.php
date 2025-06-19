511e848847d1b49cc49aa129b763ea22a0ec7718
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-06-19 11:17:37 -0300

    feat: adds throttle to the redirect route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,7 @@ use App\Http\Controllers\Web\RedirectController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('web.')->group(function (): void {
-    Route::get('/q/{hash}', RedirectController::class)->name('redirect.show');
+    Route::get('/q/{hash}', RedirectController::class)
+        ->middleware('throttle:10,1')
+        ->name('redirect.show');
 });
---
+++ tests/Feature/Http/Controllers/Api/RedirectController/RateLimitTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use App\Services\ConsistentHasher;
+
+beforeEach(function (): void {
+    $this->user = User::factory()->create();
+    $this->shards = config('shards.connections');
+    $this->payload = ['original_url' => 'https://example.com'];
+    $this->hasher = new ConsistentHasher($this->shards);
+    $this->shard = $this->hasher->getShard($this->payload['original_url']);
+});
+
+it('allows up to 10 requests per minute to /q/{hash}', function (): void {
+    $response = $this
+        ->actingAs($this->user)
+        ->postJson(route('api.short-urls.index'), $this->payload);
+
+    $hash = $response->json('data.hash');
+    $endpoint = sprintf('/q/%s', $hash);
+
+    foreach (range(1, 10) as $i) {
+        $this->get($endpoint)->assertRedirect();
+    }
+});
+
+it('blocks requests after 10 requests per minute to /q/{hash}', function (): void {
+    $response = $this
+        ->actingAs($this->user)
+        ->postJson(route('api.short-urls.index'), $this->payload);
+
+    $hash = $response->json('data.hash');
+    $endpoint = sprintf('/q/%s', $hash);
+
+    foreach (range(1, 10) as $i) {
+        $this->get($endpoint)->assertRedirect();
+    }
+
+    $this->get($endpoint)->assertTooManyRequests();
+});
+
+it('blocks requests after 10 requests if url is not found', function (): void {
+    $endpoint = sprintf('/q/%s', 'invalid');
+
+    foreach (range(1, 10) as $i) {
+        $this->get($endpoint)->assertNotFound();
+    }
+
+    $this->get($endpoint)->assertTooManyRequests();
+});
